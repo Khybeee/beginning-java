7a3d5af365ff1b86d4b4585470b277a4a38dee0b
Abbey Rempillo <willonaabbey@gmail.com>
2026-04-14 21:43:25 +0800

    Add files via upload

---
+++ Bee.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bscs241b;
+
+/**
+ *
+ * @author RempilloWillonaAbbey
+ */
+public class Bee {
+     public static void main (String [] args){
+         System.out.println("Willona Abbey Y. Rempillo");
+         System.out.println("Artistic");
+         System.out.println("Shy");
+         System.out.println("Mabait");
+         System.out.println("Tahimik");
+         System.out.println("Masipag");
+     }
+}
---
+++ extra.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bscs241b;
+
+/**
+ *
+ * @author Abbey
+ */
+public class extra 
+{
+public static void main(String[] args) {
+   
+int num1=20;
+int num2=num1/0;
+    System.out.println(""+num2);
+}
+}
